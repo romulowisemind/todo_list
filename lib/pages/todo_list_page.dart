a252cf9a0e13f864dbc0b5ab9e01d862b41c2ecb
Romulo Costa <romulocs38@gmail.com>
2022-08-22 16:07:12 -0300

    Adicionando todo_list_page

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({Key? key}) : super(key: key);
-
+//
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
