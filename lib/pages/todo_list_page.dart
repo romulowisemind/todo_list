9fa8ed7971c412f5c9c5119e8c53e36589c2d04d
Romulo Costa <romulocs38@gmail.com>
2022-08-23 09:35:01 -0300

    Adicionando todo_list_page

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
-//
+  TodoListPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      //centralizar
+    return Scaffold(
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Preço',
-              hintText: 'exemplo@exemplo.com',
-              //border: InputBorder.none,
-              errorText: null,
-              prefixText: 'R\$ ',
-              suffixText: 'cm',
-              labelStyle: TextStyle(
-                fontSize: 40,
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                ),
               ),
-            ),
-            //obscureText: true,
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              fontWeight: FontWeight.w700,
-              color: Colors.purple,
-            ),
+               const ElevatedButton(
+                onPressed: null,
+                child: Text('Entrar'),
+              ),
+            ],
           ),
         ),
       ),
